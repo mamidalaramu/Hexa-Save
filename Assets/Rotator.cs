358d24c0d3884149b25be0f500f5746e767d424d
mamidalaramu <ramukumarlucky@gmail.com>
2020-10-04 11:25:00 +0530

    Added player and Spawner
    Added player and blocks

---
+++ Assets/Rotator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward, Time.deltaTime * 30f);
+       
+    }
+}
